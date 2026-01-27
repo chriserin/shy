616f8ab6342a703975e181f3588d8d6409a4dea9
Chris Erin <chris.erin@gmail.com>
2026-01-27 17:19:20 -0500

    feat!: Switch from sqlite to duckdb

--- internal/db/db.go
+++ internal/db/db.go
@@ -1506,9 +1506,9 @@ func (db *DB) GetContextSummary(startTime, endTime int64) ([]summary.ContextSumm
 }
 
 func DbType() string {
-	if os.Getenv("SHY_DB_TYPE") == "duckdb" {
-		return "duckdb"
-	} else {
+	if os.Getenv("SHY_DB_TYPE") == "sqlite" {
 		return "sqlite"
+	} else {
+		return "duckdb"
 	}
 }
