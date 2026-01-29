6f4c410fa1dd19158f2055e313b7dfa0159f0d01
Chris Erin <chris.erin@gmail.com>
2026-01-29 15:00:31 -0500

    fix(zsh): Use line-init hook to reset index

--- cmd/integration_scripts/zsh_use.sh
+++ cmd/integration_scripts/zsh_use.sh
@@ -43,28 +43,19 @@
 # Up Arrow: Cycle through command history
 __shy_history_index=0
 
-autoload -Uz add-zle-hook-widget 2>/dev/null
-
-# Check if we should reset history index (blacklist approach)
-# Only reset on explicit "start fresh" actions, preserve index for everything else
-_shy_should_reset_history_index() {
-  echo "LASTWIDGET: $LASTWIDGET" >>~/projects/history/shy/zsh.log
-
-  # Blacklist of "start fresh" widgets
-  case "$LASTWIDGET" in
-  zle-line-init | accept-line | send-break | keyboard-quit | push-line | push-line-or-edit | get-line)
-    return 0
-    ;;
-  esac
-
-  return 1
+# Reset history index when a new line starts
+_shy_reset_history_index() {
+  __shy_history_index=0
 }
+zle -N shy-reset-history-index _shy_reset_history_index
+
+# Register the reset hook (add-zle-hook-widget allows multiple hooks without clobbering)
+autoload -Uz add-zle-hook-widget 2>/dev/null
+if typeset -f add-zle-hook-widget >/dev/null; then
+  add-zle-hook-widget zle-line-init shy-reset-history-index
+fi
 
 _shy_up_line_or_history() {
-  if _shy_should_reset_history_index; then
-    __shy_history_index=0
-  fi
-
   ((__shy_history_index++))
 
   # Get the command at the current index (1-based: 1=most recent, 2=second most recent, etc.)
@@ -81,15 +72,13 @@ _shy_up_line_or_history() {
 
   zle reset-prompt
 }
-zle -N _shy_up_line_or_history
-bindkey '^[[A' _shy_up_line_or_history # Standard up arrow
-bindkey '^[OA' _shy_up_line_or_history # Application mode up arrow
+zle -N shy-up-line-or-history _shy_up_line_or_history
+bindkey '^[[A' shy-up-line-or-history # Standard up arrow
+bindkey '^[OA' shy-up-line-or-history # Application mode up arrow
 
 # Down Arrow: Cycle forward through command history (towards more recent)
 _shy_down_line_or_history() {
-  if _shy_should_reset_history_index; then
-    __shy_history_index=0
-  elif [[ $__shy_history_index -gt 0 ]]; then
+  if [[ $__shy_history_index -gt 0 ]]; then
     ((__shy_history_index--))
   else
     BUFFER=""
@@ -111,23 +100,23 @@ _shy_down_line_or_history() {
 
   zle reset-prompt
 }
-zle -N _shy_down_line_or_history
-bindkey '^[[B' _shy_down_line_or_history # Standard down arrow
-bindkey '^[OB' _shy_down_line_or_history # Application mode down arrow
+zle -N shy-down-line-or-history _shy_down_line_or_history
+bindkey '^[[B' shy-down-line-or-history # Standard down arrow
+bindkey '^[OB' shy-down-line-or-history # Application mode down arrow
 
 alias history="shy history"
 alias fc="shy fc"
 alias r="shy fc -s"
 
 _shy_bind_viins() {
-  zvm_bindkey viins '^[[A' _shy_up_line_or_history   # Standard up arrow
-  zvm_bindkey viins '^[OA' _shy_up_line_or_history   # Application mode up arrow
-  zvm_bindkey viins '^[[B' _shy_down_line_or_history # Standard down arrow
-  zvm_bindkey viins '^[OB' _shy_down_line_or_history # Application mode down arrow
-  zvm_bindkey vicmd '^[[A' _shy_up_line_or_history   # Standard up arrow
-  zvm_bindkey vicmd '^[OA' _shy_up_line_or_history   # Application mode up arrow
-  zvm_bindkey vicmd '^[[B' _shy_down_line_or_history # Standard down arrow
-  zvm_bindkey vicmd '^[OB' _shy_down_line_or_history # Application mode down arrow
+  zvm_bindkey viins '^[[A' shy-up-line-or-history   # Standard up arrow
+  zvm_bindkey viins '^[OA' shy-up-line-or-history   # Application mode up arrow
+  zvm_bindkey viins '^[[B' shy-down-line-or-history # Standard down arrow
+  zvm_bindkey viins '^[OB' shy-down-line-or-history # Application mode down arrow
+  zvm_bindkey vicmd '^[[A' shy-up-line-or-history   # Standard up arrow
+  zvm_bindkey vicmd '^[OA' shy-up-line-or-history   # Application mode up arrow
+  zvm_bindkey vicmd '^[[B' shy-down-line-or-history # Standard down arrow
+  zvm_bindkey vicmd '^[OB' shy-down-line-or-history # Application mode down arrow
 }
 
 zvm_after_init_commands+=(_shy_bind_viins)
